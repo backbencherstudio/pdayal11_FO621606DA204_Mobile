c5d474ab2afedcd31660bdc79b04012477453408
Md Rashedul Islam <santorasu09@gmail.com>
2025-08-09 12:40:22 +0600

    chore: Nothing

--- lib/src/feature/screens/home_screen/widgets/up_coming_task_card.dart
+++ lib/src/feature/screens/home_screen/widgets/up_coming_task_card.dart
@@ -28,111 +28,76 @@ class _UpComingTaskCardState extends State<UpComingTaskCard> {
   @override
   Widget build(BuildContext context) {
     final style = Theme.of(context).textTheme;
-    return Stack(
-      children: [
-        Container(
-          width: 365.w,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [AppColor.blueSky, AppColor.purpleColor],
+    return Container(
+      padding: EdgeInsets.only(top: 5),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(12.r),
             ),
-            borderRadius: BorderRadius.circular(12.r),
-          ),
-        ),
-
-        Consumer(
-          builder: (context, ref, _) {
-            return InkWell(
-              onTap: () {
-                ref.read(selectedChapterId.notifier).state =
-                    widget.chapter!.chapterId;
-                context.push(RouteConst.todayStudyTasksScreen);
-              },
-              child: Container(
-                width: 365.w,
-                padding: EdgeInsets.all(16.r),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12.r),
-                ),
-                child: Column(
-                  children: [
-                    DayLeftSection(
-                      style: style,
-                      title: widget.chapter!.title,
-                      date: widget.chapter!.date,
-                      dayLeft: widget.chapter!.daysLeft.toString(),
-                    ),
-                    Divider(
-                      height: 1.h,
-                      color: AppColor.secondaryTextColor.withValues(alpha: 0.4),
-                    ),
-                    SizedBox(height: 24.h),
-                    TopicSection(
-                      style: style,
-                      textGradient: widget.textGradient,
-                      topicBtn: widget.chapter!.topics,
-                      topicsCount: widget.chapter!.topicsCount.toString(),
-                    ),
-                    SizedBox(height: 12.h),
-                    Divider(
-                      height: 1.h,
-                      color: AppColor.secondaryTextColor.withValues(alpha: 0.4),
-                    ),
-                    SizedBox(height: 24.h),
-                    ProgressBar(style: style),
-                    Consumer(
-                      builder: (context, ref, _) {
-                        final progress =
-                            ref.watch(progressProvider.notifier).state;
-                        return Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            GradientProgressBar(
-                              progress: progress,
-                              borderRadius: 10,
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                    SizedBox(height: 8.h),
-                    KeepGoing(style: style),
-                  ],
-                ),
+      child: Consumer(
+        builder: (context, ref, _) {
+          return InkWell(
+            onTap: () {
+              ref.read(selectedChapterId.notifier).state =
+                  widget.chapter!.chapterId;
+              context.push(RouteConst.todayStudyTasksScreen);
+            },
+            child: Container(
+              width: 365.w,
+              padding: EdgeInsets.all(16.r),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(12.r),
               ),
-            );
-          },
-        ),
-      ],
-    );
-
-    // return Container(
-    //   decoration: BoxDecoration(
-    //     gradient: LinearGradient(colors: [AppColor.blueSky, AppColor.purple]),
-    //     borderRadius: BorderRadius.circular(12.r),
-    //   ),
-    //   child: Padding(
-    //     padding: const EdgeInsets.all(24),
-    //     child: GestureDetector(
-    //       onTap: () {
-    //         context.push(RouteConst.todayStudyTasksScreen);
-    //       },
-    //       child: Container(
-    //         decoration: BoxDecoration(
-    //           color: AppColor.white,
-    //           borderRadius: BorderRadius.only(
-    //             topLeft: Radius.circular(12.r),
-    //             topRight: Radius.circular(12.r),
-    //           ),
-    //         ),
-    //         child: Padding(
-    //           padding: const EdgeInsets.all(16),
-    //           child: Text('asfdsaf'),
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
+              child: Column(
+                children: [
+                  DayLeftSection(
+                    style: style,
+                    title: widget.chapter!.title,
+                    date: widget.chapter!.date,
+                    dayLeft: widget.chapter!.daysLeft.toString(),
+                  ),
+                  Divider(
+                    height: 1.h,
+                    color: AppColor.secondaryTextColor.withValues(alpha: 0.4),
+                  ),
+                  SizedBox(height: 24.h),
+                  TopicSection(
+                    style: style,
+                    textGradient: widget.textGradient,
+                    topicBtn: widget.chapter!.topics,
+                    topicsCount: widget.chapter!.topicsCount.toString(),
+                  ),
+                  SizedBox(height: 12.h),
+                  Divider(
+                    height: 1.h,
+                    color: AppColor.secondaryTextColor.withValues(alpha: 0.4),
+                  ),
+                  SizedBox(height: 24.h),
+                  ProgressBar(style: style),
+                  Consumer(
+                    builder: (context, ref, _) {
+                      final progress =
+                          ref.watch(progressProvider.notifier).state;
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          GradientProgressBar(
+                            progress: progress,
+                            borderRadius: 10,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                  SizedBox(height: 8.h),
+                  KeepGoing(style: style),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+          );
   }
 }
